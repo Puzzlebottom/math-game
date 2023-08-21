71302f5e38a4a098ab46df299c933732ac11f1d2
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-21 11:06:14 -0400

    adds player class

---
+++ player.rb
@@ -0,0 +1,21 @@
+class Player
+  attr_reader :name, :lives, :score
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @score = 0
+  end
+
+  def lives?
+    @lives.positive?
+  end
+
+  def increment_score
+    @score += 1
+  end
+
+  def decrement_lives
+    @lives -= 1
+  end
+end
